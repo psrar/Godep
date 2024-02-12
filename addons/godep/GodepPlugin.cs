e861fe0dfb4c02abb7fda58e2853f85ccd6f3962
Pavel Simonian <pavel.sima203@gmail.com>
2024-02-12 08:41:28 +0200

    Add icons to SceneContext and ProjectContext

--- addons/godep/GodepPlugin.cs
+++ addons/godep/GodepPlugin.cs
@@ -6,17 +6,19 @@ public partial class GodepPlugin : EditorPlugin
 {
 	public override void _EnterTree()
 	{
-		// Initialization of the plugin goes here.
-		// Add the new type with a name, a parent type, a script and an icon.
 		var script = GD.Load<Script>("res://addons/godep/src/SceneContext.cs");
-		var texture = GD.Load<Texture2D>("res://addons/godep/graphics/scene_context.png");
-		AddCustomType("SceneContext", "Node", script, texture);
+		var icon = GD.Load<Texture2D>("res://addons/godep/graphics/scene_context.png");
+		AddCustomType("SceneContext", "Node", script, icon);
+
+		script = GD.Load<Script>("res://addons/godep/src/ProjectContext.cs");
+		icon = GD.Load<Texture2D>("res://addons/godep/graphics/project_context.png");
+		AddCustomType("ProjectContext", "Node", script, icon);
 	}
 
 	public override void _ExitTree()
 	{
-		// Clean-up of the plugin goes here.
 		RemoveCustomType("SceneContext");
+		RemoveCustomType("ProjectContext");
 	}
 }
 #endif
