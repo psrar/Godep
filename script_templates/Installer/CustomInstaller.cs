d42a043c743f5e62d16e39489a08de8a091a942e
Pavel Simonian <pavel.sima203@gmail.com>
2024-02-12 09:58:35 +0200

    Improvements in Installers usage and creating

---
+++ script_templates/Installer/CustomInstaller.cs
@@ -0,0 +1,23 @@
+// meta-name: Custom dependencies installer
+// meta-description: Example of installing dependencies of the project or scene context
+// meta-default: true
+
+/*
+using _BINDINGS_NAMESPACE_;
+using Godep.DI;
+
+public partial class _CLASS_ : Installer
+{
+    // Declaration of bindable fields to be set in the editor
+    // [Export] private AppConfig appConfig;
+
+    // A function called by the context to install dependencies in a container
+    public override void InstallDependencies()
+    {
+        // Player player = new() { Age = 21 };
+        // contextContainer.Bind<Player>(player);
+
+        // contextContainer.Bind(appConfig);
+    }
+}
+*/
