f751f8ec26bdf6369d4220be791d9ef9e344587e
Pavel Simonian <pavel.sima203@gmail.com>
2024-02-12 12:07:17 +0200

    Plugin paths fix

--- addons/godep/GodepPlugin.cs
+++ addons/godep/GodepPlugin.cs
@@ -6,11 +6,11 @@ public partial class GodepPlugin : EditorPlugin
 {
 	public override void _EnterTree()
 	{
-		var script = GD.Load<Script>("res://addons/godep/src/SceneContext.cs");
+		var script = GD.Load<Script>("res://addons/godep/src/contexts/SceneContext.cs");
 		var icon = GD.Load<Texture2D>("res://addons/godep/graphics/scene_context.png");
 		AddCustomType("SceneContext", "Node", script, icon);
 
-		script = GD.Load<Script>("res://addons/godep/src/ProjectContext.cs");
+		script = GD.Load<Script>("res://addons/godep/src/contexts/ProjectContext.cs");
 		icon = GD.Load<Texture2D>("res://addons/godep/graphics/project_context.png");
 		AddCustomType("ProjectContext", "Node", script, icon);
 	}
